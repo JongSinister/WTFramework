93792041f46c0a89780cff64de67fa60eec9fb9a
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2024-03-29 23:47:12 +0700

    connect to DB

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
+//Load env vars
 dotenv.config({path:'./config/config.env'});
 
-const app=express();
+//CONNECT TO DATABASE
+connectDB();
 
-app.get('/', (req,res) => {
-    res.send('<h1>Hello from express</h1>');
-    res.status(200).json({success:true,data:{id:1}});
-})
+//Body parser
+const app=express();
+app.use(express.json());
 
 const PORT=process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in', process.env.NODE_ENV,'mode on port', PORT));
+const server=app.listen(PORT, console.log('Server running in', process.env.NODE_ENV,'mode on port', PORT));
+
+//Handle unhandles promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    server.close(()=>process.exit(1));
+})
