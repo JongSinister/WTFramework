d9cbbae45c436210b1a3cc3e3ef4d6e2e957c1fd
Thiraput01 <Thiraputball@gmail.com>
2024-04-15 15:14:27 +0700

    init auth

--- Backend/server.js
+++ Backend/server.js
@@ -10,11 +10,13 @@ connectDB();
 
 //Route files
 const hotels = require('./routes/hotels');
+const auth = require('./routes/auth');
 
 //Body parser
 const app=express();
 app.use(express.json());
 app.use('/api/v1/hotels',hotels);
+app.use('/api/v1/auth', auth);
 
 const PORT=process.env.PORT || 5000;
 const server=app.listen(PORT, console.log('Server running in', process.env.NODE_ENV,'mode on port', PORT));
