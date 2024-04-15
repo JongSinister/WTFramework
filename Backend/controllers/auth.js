1151e4231747156e537fca44f427caa84979029c
Thiraput01 <Thiraputball@gmail.com>
2024-04-15 15:46:14 +0700

    add cookie

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -16,8 +16,8 @@ exports.register = async (req, res, next) =>{
         });
 
         //Create token
-        const token = user.getSignedJwtToken();
-
+        // const token = user.getSignedJwtToken();
+        sendTokenResponse(user, 200, res);
         res.status(200).json({success: true, token});
     }catch (err){
         console.log(err.stack);
@@ -53,9 +53,9 @@ exports.login = async (req, res, next) =>{
         }
     
         //create token
-        const token = user.getSignedJwtToken();
-    
-        res.status(200).json({success: true, token});
+        // const token = user.getSignedJwtToken();
+        // res.status(200).json({success: true, token});
+        sendTokenResponse(user, 200, res);
 
     }catch(err){
         return res.status(401).json({success: false, msg: 'Cannot convert email or pass to string'});
@@ -63,3 +63,21 @@ exports.login = async (req, res, next) =>{
    
 }
 
+//Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) =>{
+    //Create token
+    const token = user.getSignedJwtToken();
+
+    const options = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * (24*60*60*1000)),
+        httpOnly: true
+    }
+
+    if(process.env.NODE_ENV === 'production'){
+        options.secure = true;
+    }
+    res.status(statusCode).cookie('token', token, options).json({
+        success: true,
+        token
+    });
+}
