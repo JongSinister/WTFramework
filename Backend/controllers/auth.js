30a8deb0739daae3c1243fc0531e02623d6c8f40
Thiraput01 <Thiraputball@gmail.com>
2024-04-15 15:30:55 +0700

    user login

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -24,3 +24,42 @@ exports.register = async (req, res, next) =>{
         res.status(400).json({success: false});
     }
 }
+
+
+//@desc    Login User
+//@route   POST /api/v1/auth/login
+//@access  Public
+exports.login = async (req, res, next) =>{
+    try{
+        const {email, password} = req.body;
+
+        //Validate email and password
+        if(!email || !password){
+            return res.status(400).json({success: false,
+                 msg:'Please provide an email and password'})
+        }
+    
+        //check for user
+        const user = await User.findOne({email}).select('+password');
+        if(!user){
+            return res.status(400).json({success: false,
+                msg:'Invalid credentials'});
+        }
+        //check if password matches
+        const isMatch = await user.matchPassword(password);
+        if(!isMatch){
+            return res.status(401).json({success: false,
+                msg:'Invalid credentials'});
+        }
+    
+        //create token
+        const token = user.getSignedJwtToken();
+    
+        res.status(200).json({success: true, token});
+
+    }catch(err){
+        return res.status(401).json({success: false, msg: 'Cannot convert email or pass to string'});
+    }
+   
+}
+
