6a3919a040993db8bed575f84d576e8bf9863935
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2024-04-25 10:49:36 +0700

    Add getHotels filtering and pagination

--- Backend/controllers/hotels.js
+++ Backend/controllers/hotels.js
@@ -5,9 +5,74 @@ const Hotel = require("../models/Hotel");
 //@access   Public
 exports.getHotels = async (req, res, next) => {
   try {
-    const hotels = await Hotel.find();
-    res.status(200).json({ success: true, count: hotels.length, data: hotels });
+    //Advanced Filtering
+    let query;
+
+    //Copy req.query
+    const reqQuery = { ...req.query };
+
+    //Fields to exclude
+    const removeFields = ["select", "sort", "page", "limit"];
+
+    //Loop over remove fields and delete them from reqQury
+    removeFields.forEach((param) => delete reqQuery[param]);
+    console.log(reqQuery);
+
+    //Create query String
+    let queryStr = JSON.stringify(req.query);
+
+    //Create operators ($gt, gte, etc)
+    queryStr = queryStr.replace(
+      /\b(gt|gte|lt|lte|in)\b/g,
+      (match) => `$${match}`
+    );
+
+    //Finding resource
+    query = Hotel.find(JSON.parse(queryStr));
+
+    //Select Field
+    if (req.query.select) {
+      const fields = req.query.select.split(",").join(" ");
+      query = query.select(fields);
+    }
+    //Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("name");
+    }
+
+    //Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Hotel.countDocuments();
+
+    query = query.skip(startIndex).limit(limit);
+
+    //Executing query
+    const hotels = await query;
+    //Pagination result
+    const pagination = {};
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit,
+      };
+    }
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit,
+      };
+    }
+    res
+      .status(200)
+      .json({ success: true, count: hotels.length, pagination, data: hotels });
   } catch (err) {
+    console.log(err);
     res.status(400).json({ success: false });
   }
 };
