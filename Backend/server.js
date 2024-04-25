86b4793001cf3ce16eeaf121d7c939a1a313d7e6
Thiraput01 <Thiraputball@gmail.com>
2024-04-25 10:58:36 +0700

    xss

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const {xss} = require('express-xss-sanitizer');
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
@@ -18,6 +19,7 @@ const app=express();
 app.use(express.json());
 app.use(mongoSanitize());
 app.use(helmet());
+app.use(xss());
 app.use('/api/v1/hotels',hotels);
 app.use('/api/v1/auth', auth);
 
