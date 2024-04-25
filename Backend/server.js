106fcef5aecbd1271f2cb99fba2890c5780b9191
Thiraput01 <Thiraputball@gmail.com>
2024-04-25 10:48:44 +0700

    helmet

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const mongoSanitize = require('express-mongo-sanitize')
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
@@ -16,7 +17,7 @@ const auth = require('./routes/auth');
 const app=express();
 app.use(express.json());
 app.use(mongoSanitize());
-
+app.use(helmet());
 app.use('/api/v1/hotels',hotels);
 app.use('/api/v1/auth', auth);
 
